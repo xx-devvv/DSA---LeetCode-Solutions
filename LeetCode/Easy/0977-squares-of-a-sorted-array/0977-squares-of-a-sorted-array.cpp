4be349867efb3d1d1cc98df7426dd497d7088582
Dev <devpandey495@yahoo.com>
2026-07-17 01:29:43 +0530

    4 ms (41.32%) 31.2 MB (19.88%) 07-17-2026 0977-squares-of-a-sorted-array Easy Array

--- LeetCode/Easy/0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ LeetCode/Easy/0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -1,12 +1,41 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
+        int n = nums.size();
         vector <int> ans;
         
-        for(int i = 0 ; i < nums.size() ; i++){
-            nums[i] = nums[i]*nums[i];
+        for(int i = 0 ; i < n ; i++){
+            nums[i] *= nums[i];
         }
-        sort(nums.begin(), nums.end());
-        return nums;
+        int pivot  = 0;
+
+        for(int i = 1 ; i < n ; i++){
+            if(nums[i] < nums[pivot]) pivot = i;
+        }
+      
+        int left = pivot - 1, right  = pivot + 1;
+        ans.emplace_back(nums[pivot]);
+        while(left >= 0 && right < n){
+            if(nums[left] <= nums[right] ) {
+                ans.emplace_back(nums[left]);
+                left--;
+            }
+            else{
+                ans.emplace_back(nums[right]);
+                right++;
+            }
+        }
+
+            while(left >= 0){
+            ans.push_back(nums[left]);
+            left--;
+        }
+
+        while(right < n){
+        ans.push_back(nums[right]);
+        right++;
+}
+        return ans;        
+
     }
 };
