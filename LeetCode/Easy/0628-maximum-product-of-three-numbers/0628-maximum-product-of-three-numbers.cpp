4a882b46f0f9757ff2b9aa596da61268eaceff61
Dev <devpandey495@yahoo.com>
2026-07-26 10:10:20 +0530

    0 ms (100%) 31.7 MB (10.45%) 07-26-2026 0628-maximum-product-of-three-numbers Easy Array

--- LeetCode/Easy/0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
+++ LeetCode/Easy/0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -3,12 +3,6 @@ public:
     int maximumProduct(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         int n = nums.size();
-        if(nums.back()<0 || nums.front()>=0){
-            return nums[n-1]*nums[n-2]*nums[n-3];
-        }
-        else{
-            return max(nums[0]*nums[1]*nums[n-1],nums[n-1]*nums[n-2]*nums[n-3]);
-        }
-        return -1;
+        return max(nums[0]*nums[1]*nums[n-1],nums[n-1]*nums[n-2]*nums[n-3]);
     }
 };
