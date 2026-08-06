a0a5d482e02fab19fd44a69ec9298bec56776ff3
Dev <devpandey495@yahoo.com>
2026-08-06 15:11:55 +0530

    0 ms (100%) 8.6 MB (37.77%) 08-06-2026 3345-smallest-divisible-digit-product-i Easy Math

---
+++ LeetCode/Easy/3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
@@ -0,0 +1,18 @@
+int isDiv(int n, int t){
+    int product = 1;
+    while(n>0){
+        product *= n%10;
+        n/=10;
+    }
+    if(product%t == 0) return true;
+    return false;
+}
+class Solution {
+public:
+    int smallestNumber(int n, int t) {
+       for(int i = n ; ;n++){
+            if(isDiv(n,t)) return n;
+       }
+       return 0;
+    }
+};
