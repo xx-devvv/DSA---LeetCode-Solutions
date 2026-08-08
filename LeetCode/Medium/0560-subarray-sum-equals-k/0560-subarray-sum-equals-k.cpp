80ff8c4538772fe87f2c90209c14607683caf41a
Dev <devpandey495@yahoo.com>
2026-08-09 00:54:49 +0530

    29 ms (97.64%) 46.8 MB (34.52%) 08-09-2026 0560-subarray-sum-equals-k Medium Array

--- LeetCode/Medium/0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ LeetCode/Medium/0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int count = 0;
-
-        for (int i = 0; i < nums.size(); i++) {
-            int sum = 0;
-            for (int j = i; j < nums.size(); j++) {
-                sum += nums[j];
-                if (sum == k)
-                    count++;
-            }
-        }
-
-        return count;
+    vector<int>ps(nums.size());
+    ps[0]=nums[0];
+    for(int i = 1 ; i<nums.size() ; i++){
+    ps[i] = ps[i-1]+nums[i];
+    }
+    int count = 0;
+    unordered_map<int,int> mp;
+    for(int j = 0 ; j < nums.size() ; j++){
+        int need = ps[j]-k;
+        if(k == ps[j]) count++;
+        if(mp.find(need)!=mp.end()) count+=mp[need];
+        mp[ps[j]]++;
+    }
+    return count;    
     }
 };
