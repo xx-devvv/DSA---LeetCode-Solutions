d2e29d55b1aa1b2e2dc92864a096228c2d53fa99
Dev <devpandey495@yahoo.com>
2026-08-13 23:46:06 +0530

    75 ms (15.15%) 19.3 MB (19.7%) 08-13-2026 0003-longest-substring-without-repeating-characters Medium Hash Table

--- LeetCode/Medium/0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ LeetCode/Medium/0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -4,7 +4,7 @@ public:
         int n=s.size();
         int ans=0;
         int i=0, j=0;
-        map<char, int>mpp;
+        unordered_map<char, int>mpp;
         while(i<n && j<n){
             while(j<n && !mpp[s[j]]) mpp[s[j]]++, j++;
             ans=max(ans, j-i);
