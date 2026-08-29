49e120df653be7644326bdd9d61fd6974c358843
Dev <devpandey495@yahoo.com>
2026-08-30 00:22:33 +0530

    0 ms (100%) 7.9 MB (17.4%) 08-30-2026 0357-count-numbers-with-unique-digits Medium Math

--- LeetCode/Medium/0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.cpp
+++ LeetCode/Medium/0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
-    int countNumbersWithUniqueDigits(int n) {
-        if(n==1){
-return 10;}
-if(n==2) return 91;
-if(n==3) return 739;
-if(n==4) return 5275;
-if(n==5) return 32491;
-if(n==6) return 168571;
-if(n==7) return 712891;
-if(n==8) return 2345851;
-return 1;
+    long long perm(int n,int r){
+        long long result=1;
+        for(int i=0;i<r;i++){
+            result*=(n-i);
+        }
+        return result;
+    }
+    int countNumbersWithUniqueDigits(int n){
+        vector<int>dp(n+1,0);
+        dp[0]=1;
+        for(int i=1;i<=n;i++){
+            dp[i]+=dp[i-1]+9*perm(9,i-1);
+        }
+        return dp[n];
     }
 };
